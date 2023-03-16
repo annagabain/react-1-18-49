144fae9c3eadae01de961fd01a39479766c45923
Anna Gabain <anna.gabain@outlook.com>
2023-03-16 20:36:34 +0000

    Handling Events in React

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import EventsFunctional from "./components/EventsFunctional";
+import EventsClass from "./components/EventsClass";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <h1>Events</h1>
+    <EventsFunctional/>
+    <EventsClass/>
+  </div>;
 }
 
 export default App;
