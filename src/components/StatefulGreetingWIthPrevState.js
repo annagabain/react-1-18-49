7710a966a57d57ce27ee08f7f3c5afe82d6f4f1d
Anna Gabain <anna.gabain@outlook.com>
2023-03-23 19:35:57 +0000

    Updating the State, part 3

--- src/components/StatefulGreetingWIthPrevState.js
+++ src/components/StatefulGreetingWIthPrevState.js
@@ -6,20 +6,40 @@ class StatefulGreetingWithPrevState extends React.Component {
     this.state = {
       introduction: "Hello!",
       buttonText: "Exit",
+      count: 0,
     };
   }
 
   handleClick() {
     this.setState(
       {
-        introduction: "Goodbye!",
-        buttonText: "Enter",
+        introduction: this.state.introduction ==="Hello!" ? "Goodbye!" : "Hello!",
+        buttonText: this.state.buttonText === "Exit" ? "Enter" : "Exit",
+        // count: this.state.count + 1
       },
       () => {
         console.log("new state", this.state.introduction);
         console.log("new state", this.state.buttonText);
+        // console.log("new count", this.state.count);
       }
     );
+  }
+
+  increment() {
+    this.setState({
+      count: this.state.count +1
+    })
+    console.log(this.state.count)
+  }
+
+  incrementFive(){
+    this.increment()
+    this.increment()
+    this.increment()
+    this.increment()
+    this.increment()
+    console.log()
+
   }
 
   render() {
@@ -31,6 +51,11 @@ class StatefulGreetingWithPrevState extends React.Component {
         <button onClick={() => this.handleClick()}>
           {this.state.buttonText}
         </button>
+        <button onClick={() => this.increment()}>
+        {/* <button onClick={() => this.incrementFive()}> */}
+        Increment
+          </button>
+          <p>You have clickd {this.state.count} times</p>
       </div>
     );
   }
